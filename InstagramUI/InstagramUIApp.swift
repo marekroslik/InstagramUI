2b89cc6fd41c714f983be33b3f0671d4295dd0ea
Marek Roslik <marek.roslik@gmail.com>
2022-02-03 18:31:37 +0300

    Initial Commit

---
+++ InstagramUI/InstagramUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUIApp.swift
+//  InstagramUI
+//
+//  Created by Marek Roslik on 3.02.22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
